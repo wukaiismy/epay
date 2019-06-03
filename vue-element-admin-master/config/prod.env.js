0c43dbc88d92ca9f185a2ad6c49dead0bc366df8
hanks <37804662+wukaiismy@users.noreply.github.com>
2019-06-03 11:24:06 +0800

    Update prod.env.js

--- vue-element-admin-master/config/prod.env.js
+++ vue-element-admin-master/config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://47.107.45.63:80"'
+  BASE_API: '"http://127.0.0.1:80"'
   // BASE_API: '"http://192.168.1.28:8001/api/v1/"'
 };
