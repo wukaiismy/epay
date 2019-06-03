0b6ac95caf4a2f5623720d8f0ce79f2f58024d81
hanks <37804662+wukaiismy@users.noreply.github.com>
2019-06-03 11:23:33 +0800

    change ip
    change the ip

--- vue-element-admin-master/config/dev.env.js
+++ vue-element-admin-master/config/dev.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   // BASE_API: '"http://192.168.1.28:8001/api/v1/"'
-  BASE_API: '"http://47.107.45.63:80"'
+  BASE_API: '"http://127.0.0.1:80"'
 
   // BASE_API: '"https://api-dev"'
 };
