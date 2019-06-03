9c206e4f0ef1b73d9c61e66ed7431a037bf22dee
hanks <37804662+wukaiismy@users.noreply.github.com>
2019-06-03 11:22:41 +0800

    ip changes
    change the ip

--- vue-element-admin-master/config/index.js
+++ vue-element-admin-master/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     proxyTable: {
       "/backend": {
         // 测试环境
-        target: "http://47.107.45.63", // 接口域名
+        target: "http://127.0.0.1", // 接口域名
         changeOrigin: true, //是否跨域
         pathRewrite: {
           "^/backend": "/backend" //需要rewrite重写的,
